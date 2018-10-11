da6962e60a312f7386965db8a3ad6c658d57127c
liuzhe <zhethink@gmail.com>
2018-10-11 09:55:08 +0800

    复制并改名一个可用

--- baseCopyImg.js
+++ baseCopyImg.js
@@ -1,8 +1,9 @@
 let fs = require('fs');
 let path = require('path');
-let fileName = '123.jpg'
-let sourceFile = path.join(__dirname+'/public/upload/tt01/', fileName)
-let destPath = path.join(__dirname, "/public/upload/tt02/", fileName)
+let fileName = 'thumb_2018101119980407.jpg';
+let newName = fileName.substring(6);
+let sourceFile = path.join(__dirname+'/images/906/', fileName)
+let destPath = path.join(__dirname, "/images/906/", newName)
 console.log(__dirname)
 let readStream = fs.createReadStream(sourceFile)
 let writeStream = fs.createWriteStream(destPath)
