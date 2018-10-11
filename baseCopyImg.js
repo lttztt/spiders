8e0113ce88867bf6987f7c17a9136a76eedbe4af
liuzhe <zhethink@gmail.com>
2018-10-11 16:16:06 +0800

    完成执行baseCopyImg.js 复制所有目录的图片

--- baseCopyImg.js
+++ baseCopyImg.js
@@ -1,18 +1,29 @@
 let fs = require('fs');
 let path = require('path');
 
-var dir = '901'
-console.log("打开"+ dir +"目录");
-fs.readdir("./images/" + dir,function(err, files){
-   if (err) {return console.error(err);}
-   // console.log(files)
-   console.log('开始批量复制');
-   files = files.filter(name => name.length > 10);
-   // console.log(files)
-    for (var i = files.length - 1; i >= 0; i--) {
-    	copyImg(dir, files[i]);
-    }
+fs.readdir("./images",function(err, files){
+   if (err) {
+       return console.error(err);
+   }
+   var arr = files.filter((item)=>{return item.length === 3});
+   console.log(arr);
+   for (var i = arr.length - 1; i >= 0; i--) {
+   	myCopy(arr[i]);
+   }
 });
+
+var myCopy = function(dir){
+	console.log("打开"+ dir +"目录");
+	fs.readdir("./images/" + dir,function(err, files){
+	   if (err) {return console.error(err);}
+	   console.log('开始批量复制');
+	   files = files.filter(name => name.length > 10);
+	    for (var i = files.length - 1; i >= 0; i--) {
+	    	copyImg(dir, files[i]);
+	    }
+	});
+}
+
 
 
 
